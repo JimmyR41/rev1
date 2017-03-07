6bfe0923c71ab4728643cdb1520587dfd0006bd9
Jimmy Riccelli <jrriccelli@gmail.com>
2017-03-06 20:46:11 -0500

    done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,16 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+
+  end
+
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -11,6 +21,23 @@ class ArticlesController < ApplicationController
     else
       render "new"
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if  @article.update(article_params)
+      flash[:notice] = "Article updated!"
+      redirect_to article_path(@article)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+      flash[:notice] = "Successfully Deleted"
+      redirect_to articles_path
   end
 
   def show
